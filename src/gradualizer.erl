ac9ae5d31d89839e38b663b24b1287d49fd88664
Robert Fiko <fiko.robert@gmail.com>
2025-05-23 16:39:10 +0200

    clean up

--- src/gradualizer.erl
+++ src/gradualizer.erl
@@ -62,8 +62,7 @@
          type_check_module/1, type_check_module/2,
          type_check_dir/1, type_check_dir/2,
          type_check_files/1, type_check_files/2,
-         type_check_forms/2,
-        make_atom/1]).
+         type_check_forms/2]).
 
 -export([type/1,
          env/0, env/1, env/2,
@@ -151,8 +150,6 @@
 -include("gradualizer.hrl").
 -include("typechecker.hrl").
 
-make_atom(Data) ->
-    list_to_atom(Data).
 
 %% API functions
 
